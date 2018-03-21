5231814c3e1015e995d3e69766d97950e16a4868
Marcin Pham <marcin.kamo@gmail.com>
2018-03-21 15:56:30 +0100

    Included exception handling evaluation test

--- src/Tests/EvaluationTest.java
+++ src/Tests/EvaluationTest.java
@@ -3,15 +3,17 @@ import PicrossSolver.*;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.InputMismatchException;
 import java.util.LinkedList;
 import java.util.List;
 
 public class EvaluationTest {
     @Test
     public void CorrectEvaluation(){
-        List<Integer> data = new LinkedList<>();
+
 
         //Tested pattern: 2,3,1,1,3,2 in length 17: should unequivocally result in combination: 11011101010111011
+        List<Integer> data = new LinkedList<>();
         data.add(2);
         data.add(3);
         data.add(1);
@@ -47,5 +49,25 @@ public class EvaluationTest {
         assertEquals(false, row.get(14).getValue());
         assertEquals(true, row.get(15).getValue());
         assertEquals(true, row.get(16).getValue());
+    }
+
+    @Test
+    void ExceptionTesting(){
+        //Testing pattern: 1 against row 1xxx1
+        List<Integer> data = new LinkedList<>();
+        data.add(1);
+        iPattern pat = new PatternCode(data);
+        List<Cell> row = new LinkedList<>();
+        for(int i=0; i<5; i++)
+            row.add(new Cell());
+
+        row.get(0).setCertain(true);
+        row.get(4).setCertain(true);
+        row.get(0).setValue(true);
+        row.get(0).setValue(true);
+
+        example(() -> Pool.evaluate(pat, row));
+
+        Throwable exception = assertThrows(InputMismatchException.class, Pool::evaluate);
     }
 }
